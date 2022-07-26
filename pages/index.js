9eba055aea7daa32825a9b89137ee827cfe0e5a9
Emmanuel Okereke <eokereke47@gmail.com>
2022-07-26 13:21:36 +0100

    index.tsx to js

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,33 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-
+import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
-import {FeaturedPosts} from '../sections'
 
-const posts =[
-  {title: "react testing", excerpt: "learn react testing"},
-  {title: "react with tailwind", excerpt: "learn react with tailwing"},
-  {title: "react html", excerpt: "learn react html"}
-
-]
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>BLOG APP</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <FeaturedPosts/>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
-            <PostCard post={post.node} key={post.title} />
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
-
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
-
 
 // Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
-
   return {
-    props: { posts }
+    props: { posts },
   };
 }
+
